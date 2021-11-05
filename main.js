a3a88223db711bfc91804900c65acac5e0b4c88d
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-11-05 16:59:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function next2(){
+    window.location="index1.html";
+}
+function next3(){
+    window.location="index2.html";
+}
+function next4(){
+    window.location="index3.html";
+}
+function next5(){
+    window.location="index4.html";
+}
+function next6(){
+    window.location="index5.html";
+}
---
+++ main4.js
@@ -0,0 +1,30 @@
+var status1="";
+img="";
+function preload(){
+img=loadImage('dog_cat.jpg');
+}
+function setup(){
+canvas=createCanvas(640,420);
+canvas.center();
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="status:detecting object";
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+status1=true;
+objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+}
+function draw(){
+image(img,0,0,640,420);
+fill('#ff0000');
+text("pen",45,75);
+noFill();
+stroke("#ff0000");
+rect(30,60,450,350);
+}
